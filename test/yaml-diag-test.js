0b261b962296e7349fc0f411d2d589d7b0cbf763
Taylor Beseda <tbeseda@gmail.com>
2023-10-26 11:44:58 -0600

    eslint works in Node 14

--- test/yaml-diag-test.js
+++ test/yaml-diag-test.js
@@ -1,4 +1,6 @@
+// import { Readable } from 'stream'
 import test from 'tape'
+// import TapReader from '../src/index.js'
 
 test('TapReader: YAML diag', { skip: true }, t => {
   t.fail('YAML diag')
